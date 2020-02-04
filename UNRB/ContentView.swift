e9b12373551b980c6cf81e8170e33018566bf63b
Carlos Varela <carlosalexvarela@gmail.com>
2020-02-04 15:21:26 -0800

    Initial Commit

---
+++ UNRB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UNRB
+//
+//  Created by Carlos Varela on 2/4/20.
+//  Copyright © 2020 Carlos Varela. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
