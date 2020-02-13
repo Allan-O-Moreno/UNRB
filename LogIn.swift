627b9d10a733cea050fbc291e5cca3bbbd64f51c
Allan Moreno <allanm@nevada.unr.edu>
2020-02-13 12:17:03 -0800

    Initial Log in page Set up

---
+++ LogIn.swift
@@ -0,0 +1,43 @@
+//
+//  LogIn.swift
+//  UNR_Classifier
+//
+//  Created by Allan Moreno on 2/13/20.
+//  Copyright © 2020 Allan Moreno. All rights reserved.
+//
+
+import SwiftUI
+
+struct LogIn: View {
+    @State private var email: String = "Please Enter Email"
+    @State private var password: String = "Please Enter Password"
+    var body: some View {
+        
+        VStack {
+                Text("Login page")
+                
+                HStack {
+                    
+                    Text("Email: ")
+                    TextField("Please enter your email", text: $email)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                } // end HStack
+                
+                HStack{
+                Text("Password: ")
+                    TextField("Please enter your password", text: $password)
+                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 3)
+                } // end Hstack
+                
+            } // end VStack
+        
+        
+        
+    }
+}
+
+struct LogIn_Previews: PreviewProvider {
+    static var previews: some View {
+        LogIn()
+    }
+}
