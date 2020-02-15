4345cb16f981ab80bde10cc3bec51a5555510e90
Timothy Kashi <timothyk5@yahoo.com>
2020-02-15 11:24:28 -0800

    settings page

---
+++ Settings.swift
@@ -0,0 +1,21 @@
+//
+//  Settings.swift
+//  UNRB
+//
+//  Created by Timothy Kashi on 2/13/20.
+//  Copyright © 2020 Carlos Varela. All rights reserved.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
